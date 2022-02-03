5c9f9927f65a0efff56413139d3d2f6d8b97e29a
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-02-03 11:08:59 +0100

    fix error config

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 /* your code in typescript */
+const root = window.document.getElementById('root') as HTMLParagraphElement
+if (root !== null) root.innerHTML = `<p>Ciao Mondo</p>`
+
+console.log('ciao mondo')
