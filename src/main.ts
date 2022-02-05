e4db07d988f26dc38856f3901751a66a1ebfd4da
Maurizio Tolomeo <37340833+moris88@users.noreply.github.com>
2022-02-05 13:14:35 +0100

    add creaP, creaDiv and creaTable

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 /* your code in typescript */
-const root = window.document.getElementById('root') as HTMLParagraphElement
-if (root !== null) root.innerHTML = `<p>Ciao Mondo</p>`
 
-console.log('ciao mondo')
+import { Graphic } from './lib/Graphic/graphic'
+
+const root = window.document.getElementById('root') as HTMLElement
+
+const div = Graphic.creaDiv(root)
+let p: HTMLParagraphElement | null
+if (div !== null) {
+    p = Graphic.creaP(div, 'ciao Mondo')
+    if (p !== null) {
+        Graphic.inserisciChildDiv(div, p)
+    }
+}
+
+const table = Graphic.creaTable(root, ['id', 'nome', 'cognome'], {
+    classes: 'table',
+})
+
+const riga = table.recordsID
+
+if (riga !== null) {
+    Graphic.inserisciRecordsTable(riga, ['1', 'Maurizio', 'Tolomeo'])
+    Graphic.inserisciRecordsTable(riga, ['2', 'Cecilia', 'Ferrulli'])
+}
