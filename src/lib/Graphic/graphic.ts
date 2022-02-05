ecde899075084c54e30b13b370e238b55b8abda7
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-02-05 18:40:20 +0100

    add other tag

--- src/lib/Graphic/graphic.ts
+++ src/lib/Graphic/graphic.ts
@@ -3,64 +3,211 @@ export namespace Graphic {
         classes?: string
         disabled?: boolean
     }
+    export interface ElementTag {
+        elementID: string
+        elementString: string
+    }
 
-    export function creaP(
-        parent: HTMLElement,
-        text: string,
-        other?: HTMLObject
-    ) {
+    export interface TableTag {
+        theadID?: string
+        tbodyID?: string
+    }
+
+    export type TagName =
+        | 'p'
+        | 'span'
+        | 'div'
+        | 'table'
+        | 'a'
+        | 'select'
+        | 'option'
+        | 'tr'
+        | 'input'
+    export type Tag = {
+        nameTag: TagName
+    } & ElementTag &
+        TableTag
+
+    export function inserisciTagHTML(parent: HTMLElement | null, child: Tag) {
+        if (parent !== null) parent.innerHTML += `${child.elementString}`
+    }
+
+    export function recuperaTagHTML(id: string) {
+        return window.document.getElementById(id)
+    }
+
+    export function creaParagrafo(text: string, other?: HTMLObject): Tag {
         const id = 'p_' + generateRandomString(10)
-        parent.innerHTML += `<p id="${id}" class="${other?.classes}">${text}</p>`
-        return window.document.getElementById(id) as HTMLParagraphElement
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        return {
+            nameTag: 'p',
+            elementID: id,
+            elementString: `<p id="${id}" ${className}>${text}</p>`,
+        }
     }
 
-    export function inserisciChildDiv(parent: HTMLDivElement,child: HTMLElement){
-        parent.innerHTML += `${child}`
+    export function creaSpan(text: string, other?: HTMLObject): Tag {
+        const id = 'span_' + generateRandomString(10)
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        return {
+            nameTag: 'span',
+            elementID: id,
+            elementString: `<span id="${id}" ${className}>${text}</span>`,
+        }
     }
 
-    export function creaDiv(
-        parent: HTMLElement,
-        other?: HTMLObject
-    ) {
+    export function creaDiv(other?: HTMLObject): Tag {
         const id = 'div_' + generateRandomString(10)
-        parent.innerHTML += `<div id="${id}" class="${other?.classes}"></div>`
-        return window.document.getElementById(id) as HTMLDivElement
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        return {
+            nameTag: 'div',
+            elementID: id,
+            elementString: `<div id="${id}" ${className}></div>`,
+        }
     }
 
-    export function creaTable(
-        parent: HTMLElement,
-        fields: string[],
-        other?: HTMLObject
-    ) {
+    export function creaTable(fields: string[], other?: HTMLObject): Tag {
+        const className = other?.classes ? `class="${other?.classes}"` : ``
         const tableID = 'table_' + generateRandomString(10)
+        const fieldsID = 'thead_' + generateRandomString(10)
         const recordsID = 'tbody_' + generateRandomString(10)
-        let result = `<table id="${tableID}" class="${other?.classes}"><thead><tr>`
+        let result = `<table id="${tableID}" ${className}><thead id="${fieldsID}"><tr>`
         for (const field of fields) {
             result += `<th>${field}</th>`
         }
         result += `</tr></thead><tbody id="${recordsID}"></tbody></table>`
-        parent.innerHTML += result
         return {
-            tableID: window.document.getElementById(tableID),
-            recordsID: window.document.getElementById(recordsID),
+            nameTag: 'table',
+            elementID: tableID,
+            elementString: result,
+            theadID: fieldsID,
+            tbodyID: recordsID,
         }
     }
 
-    export function inserisciRecordsTable(
-        tableRecord: HTMLElement,
+    export function inserisciRecord(
+        parentID: string,
         records: string[],
         other?: HTMLObject
-    ) {
-        let result = `<tr class="${other?.classes}">`
+    ): Tag {
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const rowID = 'tr_' + generateRandomString(10)
+        let result = `<tr id="${rowID}" ${className}>`
         for (const record of records) {
             result += `<td>${record}</td>`
         }
         result += `</tr>`
-        tableRecord.innerHTML += result
+        const riga = window.document.getElementById(parentID)
+        if (riga !== null) riga.innerHTML += result
+        return {
+            nameTag: 'tr',
+            elementID: rowID,
+            elementString: result,
+        }
     }
 
-    export function pulisciRecordsTable(tableRecord: HTMLElement){
-        tableRecord.innerHTML = ''
+    export function pulisciRecords(table: Tag) {
+        const tableRecord = window.document.getElementById(table.elementID)
+        if (tableRecord !== null) tableRecord.innerHTML = ''
+    }
+
+    export function creaLink(
+        text: string,
+        href?: string,
+        target?: string,
+        other?: HTMLObject
+    ): Tag {
+        const id = 'a' + generateRandomString(10)
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const linkHref = href ? `href="${href}"` : ``
+        const linkTarget = target ? `target="${target}"` : ``
+        return {
+            nameTag: 'a',
+            elementID: id,
+            elementString: `<a id="${id}" ${className} ${linkHref} ${linkTarget}>${text}</a>`,
+        }
+    }
+
+    export function creaSelezione(other?: HTMLObject): Tag {
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const selectID = 'select_' + generateRandomString(10)
+        let result = `<select id="${selectID}" ${className}></select>`
+        return {
+            nameTag: 'select',
+            elementID: selectID,
+            elementString: result,
+        }
+    }
+
+    export function inserisciOptione(
+        selectID: string,
+        label: string,
+        value: any,
+        selected: boolean,
+        other?: HTMLObject
+    ): Tag {
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const optionID = 'option_' + generateRandomString(10)
+        let result = selected
+            ? `<option id="${optionID}" ${className} selected value="${value}">${label}</option>`
+            : `<option id="${optionID}" ${className} value="${value}">${label}</option>`
+        const select = window.document.getElementById(selectID)
+        if (select !== null) select.innerHTML += result
+        return {
+            nameTag: 'option',
+            elementID: optionID,
+            elementString: result,
+        }
+    }
+
+    export function creaInput(
+        type: string,
+        name: string,
+        label: string,
+        other?: HTMLObject
+    ): Tag {
+        const id = 'input_' + generateRandomString(10)
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const typeInput = type ? `type="${type}"` : ``
+        const nameInput = name ? `name="${name}"` : ``
+        const valueInput = label ? `value="${label}"` : ``
+        return {
+            nameTag: 'input',
+            elementID: id,
+            elementString: `<input id="${id}" ${className} ${typeInput} ${nameInput} ${valueInput}></input>`,
+        }
+    }
+
+    export function creaButton(
+        label: string,
+        other?: HTMLObject
+    ): Tag {
+        const id = 'input_' + generateRandomString(10)
+        const className = other?.classes ? `class="${other?.classes}"` : ``
+        const valueButton = label ? `class="${label}"` : ``
+        return {
+            nameTag: 'input',
+            elementID: id,
+            elementString: `<button id="${id}" ${className}>${valueButton}</button>`,
+        }
+    }
+
+    export type TypeEvent =
+        | 'click'
+        | 'load'
+        | 'change'
+        | 'blur'
+        | 'focus'
+        | 'close'
+        | 'dblclick'
+        | 'keypress'
+    export function addEvent(
+        parentID: string,
+        typeEvent: TypeEvent,
+        callback: (this: HTMLElement, ev: Event | MouseEvent) => any
+    ) {
+        const element = window.document.getElementById(parentID)
+        if (element !== null) element.addEventListener(typeEvent, callback)
     }
 }
 
