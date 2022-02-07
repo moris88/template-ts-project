ed8189a58b272fc3dbc91a71f807423a1bf5be54
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-02-07 08:54:33 +0100

    test file Graphic

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,4 @@
 /* your code in typescript */
 
-import { Graphic } from './lib/Graphic/graphic'
-
-const root = window.document.getElementById('root') as HTMLElement
-
-const div = Graphic.creaDiv(root)
-let p: HTMLParagraphElement | null
-if (div !== null) {
-    p = Graphic.creaP(div, 'ciao Mondo')
-    if (p !== null) {
-        Graphic.inserisciChildDiv(div, p)
-    }
-}
-
-const table = Graphic.creaTable(root, ['id', 'nome', 'cognome'], {
-    classes: 'table',
-})
-
-const riga = table.recordsID
-
-if (riga !== null) {
-    Graphic.inserisciRecordsTable(riga, ['1', 'Maurizio', 'Tolomeo'])
-    Graphic.inserisciRecordsTable(riga, ['2', 'Cecilia', 'Ferrulli'])
-}
+import { TestGraphic } from './test'
+TestGraphic.main()
---
+++ src/test.ts
@@ -0,0 +1,57 @@
+/* your code in typescript */
+import { Graphic } from './lib/Graphic/graphic'
+import { Classes } from './lib/Classes/classes'
+
+export namespace TestGraphic {
+
+    export function main() { 
+
+        const root = Graphic.recuperaTagHTML('root')
+        const divTag = Graphic.creaDiv()
+        const pTag = Graphic.creaParagrafo('ciao Mondo', {
+            classes: Classes.add(['text-center']),
+        })
+        Graphic.inserisciTagHTML(root, divTag)
+        const div = Graphic.recuperaTagHTML(divTag.elementID)
+        Graphic.inserisciTagHTML(div, pTag)
+
+        const tableTag = Graphic.creaTabella(['id', 'nome', 'cognome'], {
+            classes: Classes.add(['table']),
+        })
+        Graphic.inserisciTagHTML(root, tableTag)
+
+        Graphic.inserisciRecord(tableTag.elementID, ['1', 'Maurizio', 'Tolomeo'])
+        Graphic.inserisciRecord(tableTag.elementID, ['2', 'Cecilia', 'Ferrulli'])
+
+        const linkTag = Graphic.creaLink('clicca qui', 'https://www.google.it')
+        Graphic.inserisciTagHTML(root, linkTag)
+
+        const selectTag = Graphic.creaSelezione({
+            classes: Classes.add(['form-control']),
+        })
+        Graphic.inserisciTagHTML(root, selectTag)
+        Graphic.inserisciOptione(selectTag.elementID, 'maschio', 'm', false)
+        Graphic.inserisciOptione(selectTag.elementID, 'femmina', 'f', true)
+
+        const btnTag = Graphic.creaInput('button', 'btn', 'apri')
+        Graphic.inserisciTagHTML(root, btnTag)
+        Graphic.addEvent<HTMLButtonElement>(
+            btnTag.elementID,
+            'click',
+            function (event) {
+                console.log(event)
+                window.alert('ciao')
+            }
+        )
+
+        Graphic.addEvent<HTMLSelectElement>(
+            selectTag.elementID,
+            'change',
+            function (event) {
+                const target = event.target as HTMLSelectElement
+                console.log(target.value)
+            }
+        )
+
+    }
+}
