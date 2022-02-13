76af469b5de44bede04a0a120ef8575605231083
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-02-13 17:15:36 +0100

    Update classes.ts

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -1,18 +1,30 @@
 export namespace Classes {
     let array: string[] = []
 
-    export function add (classes: string[]) {
-      array = [...classes]
-      return get()
+    export function add(classes: string[]) {
+        array = [...classes]
+        return print()
     }
 
-    export function get () {
-      let result = ''
-      if (array.length === 0) result += ''
-      if (array.length === 1) result += `${array[0]}`
-      else {
-        for (const elem of array) result += `${elem} `
-      }
-      return result
+    export function get(name: string) {
+        return array.find((elem) => elem === name)
+    }
+
+    export function remove(name: string) {
+        array = array.filter((elem) => elem !== name )
+    }
+
+    export function removeAll() {
+        array = []
+    }
+
+    export function print() {
+        let result = ''
+        if (array.length === 0) result += ``
+        if (array.length === 1) result += `${array[0]}`
+        else {
+            for (const elem of array) result += `${elem} `
+        }
+        return result
     }
 }
